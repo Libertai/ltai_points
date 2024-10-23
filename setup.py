16f939a5f1ca1258eb935f49c2c69c3aee2be688
aliel <ali.elb@gmail.com>
2024-10-24 00:09:58 +0200

    add pyyaml dep

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -16,7 +16,8 @@ requirements = [
     'plyvel>=1.5.0',
     'python-dotenv',
     'setuptools',
-    'web3'
+    'web3',
+    'pyyaml'
 ]
 
 test_requirements = [ ]
